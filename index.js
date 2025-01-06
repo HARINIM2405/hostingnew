897967b76928de6c67c1c982bef279796cf9fa0d
HARINIM2405 <149925922+HARINIM2405@users.noreply.github.com>
2025-01-06 10:22:40 +0530

    edit done

--- index.js
+++ index.js
@@ -4,25 +4,30 @@ import mongoose from "mongoose";
 import bodyParser from "body-parser";
 import router from "./routes/userRoute.js";
 import cors from "cors";
-const app=express();
 
-//bodypraser is a middleware for parsing the json  data
+const app = express();
 
-app.use(bodyParser.json())
+// bodyParser is a middleware for parsing the JSON data
+app.use(bodyParser.json());
 app.use(cors());
+
 dotenv.config();
 
-const PORT =process.env.PORT || 5000;
-const MONGO_URL=process.env.MONGO_URL;
+const PORT = process.env.PORT || 5000;
+const MONGO_URL = process.env.MONGO_URL;
 
-app.use('/api/user',router)
+// Use the user route
+app.use('/api/user', router);
 
-
-mongoose
-         .connect(MONGO_URL)
-         .then(()=>{
-            console.log("db connected");
-            app.listen(PORT,()=>{
-                console.log(`Server is running on port ${PORT}`)
-            })
-         })
+// MongoDB connection and error handling
+mongoose.connect(MONGO_URL)
+  .then(() => {
+    console.log("DB connected");
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.error("DB connection failed", error);
+    process.exit(1);  // Exit the process with failure code
+  });
